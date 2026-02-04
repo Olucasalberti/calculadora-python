3bb5f9cfa36ac14c462acb71ebb851f03bd5bfa9
Olucasalberti <albertimachado@hotmail.com>
2026-02-03 22:57:05 -0300

    Create calculadora.py
    Primeiro projeto em Python, criado para estudo e prática. Este repositório servirá como registro da minha evolução ao longo do aprendizado.

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+while True:
+
+    print('Vamos começar o calculo! Para começar escolha umas das operações a seguir:')
+    print('1 - SOMA')
+    print('2 - SUBTRAÇÃO')
+    print('3 - MULTIPLICAÇÃO')
+    print('4 - DIVISÃO')
+
+    operacao = int(input('add o valor:'))
+
+    num1 = int(input('insira o primeiro valor:'))
+    num2 = int(input('insira o segundo valor:'))
+
+    if operacao == 1:
+        print('A soma de seus valores é:', num1 + num2)
+
+    elif operacao == 2:
+        print('A subtração de seus valores é:', num1 - num2)
+
+    elif operacao == 3:
+        print('A multiplicação de seus valores é:', num1 * num2)
+
+    elif operacao == 4:
+        if num2 == 0:
+            print('Não é possivel dividir por zero')
+        else:
+            print('A divisão de seus valores é:', num1 / num2)
+
+    else:
+        print('Operação invalida!')
